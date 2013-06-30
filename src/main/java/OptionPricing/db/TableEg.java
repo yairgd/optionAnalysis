43500f4135c4a35034b1536a2716343cebf8db02
yair <yair@localhost>
2013-06-30 03:48:43 +0300

    read amd option table, works fine

--- src/main/java/OptionPricing/db/TableEg.java
+++ src/main/java/OptionPricing/db/TableEg.java
@@ -1,4 +1,5 @@
 package OptionPricing.db;
+
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,39 +7,37 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TableEg {
-   public static void main(String[] args) {
-      //String html = "http://publib.boulder.ibm.com/infocenter/iadthelp/v7r1/topic/" +
+
+    public static void main(String[] args) {
+        //String html = "http://publib.boulder.ibm.com/infocenter/iadthelp/v7r1/topic/" +
         //    "com.ibm.etools.iseries.toolbox.doc/htmtblex.htm";
-      
-      String html = "http://finance.yahoo.com/q/op?s=AMD+Options";
-      try {
-         Document doc = Jsoup.connect(html).get();
-         Elements callOptionTable = doc.select("table.yfnc_datamodoutline1");
-     
-         Elements tableElements = callOptionTable.select("table.yfnc_datamodoutline1");//callOptionTable.select("table.yfnc_datamodoutline1") ;//doc.select("table.yfnc_datamodoutline1");
 
-         Elements tableHeaderEles = tableElements.select("thead tr th");
-         System.out.println("headers");
-         for (int i = 0; i < tableHeaderEles.size(); i++) {
-            System.out.println(tableHeaderEles.get(i).text());
-         }
-         System.out.println();
+        String html = "http://finance.yahoo.com/q/op?s=AMD+Options";
+        try {
 
-         Elements tableRowElements = tableElements.select(":not(thead) tr");
 
-         for (int i = 0; i < tableRowElements.size(); i++) {
-             System.out.print("row #"+i+"\n--------\n");
-            Element row = tableRowElements.get(i);
-            Elements rowItems = row.select("td");
-            for (int j = 1; j < rowItems.size(); j++) {
-               System.out.println(rowItems.get(j).text());
+            Document doc = Jsoup.connect(html).get();
+
+            for (Element table : doc.select("table[class=yfnc_datamodoutline1]")) {
+                
+
+                Elements tableHeaderEles = table.select("th");
+                System.out.println("headers");
+                for (int i = 0; i < tableHeaderEles.size(); i++) {
+                    System.out.println(tableHeaderEles.get(i).text());
+                }
+
+                for (Element row : table.select("tr")) {
+                    Elements tds = row.select("td");
+                    if (tds.size() > 6) {
+                        System.out.println(tds.get(0).text() + ":" + tds.get(1).text());
+                    }
+                }
             }
-            System.out.println();
-            System.out.println();
-         }
 
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
