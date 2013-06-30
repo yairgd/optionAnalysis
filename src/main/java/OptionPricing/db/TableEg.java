29adc57ccbccb8e5a8422ae7b991495f53adf3bf
yair <yair@localhost>
2013-07-01 02:30:36 +0300

    initial version for fetchCSV

--- src/main/java/OptionPricing/db/TableEg.java
+++ src/main/java/OptionPricing/db/TableEg.java
@@ -1,6 +1,18 @@
 package OptionPricing.db;
 
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,6 +24,11 @@ public class TableEg {
         //String html = "http://publib.boulder.ibm.com/infocenter/iadthelp/v7r1/topic/" +
         //    "com.ibm.etools.iseries.toolbox.doc/htmtblex.htm";
 
+        
+        
+        TableEg tableEg =  new TableEg();
+        tableEg.fetchCSV();
+        
         String html = "http://finance.yahoo.com/q/op?s=AMD+Options";
         try {
 
@@ -19,7 +36,7 @@ public class TableEg {
             Document doc = Jsoup.connect(html).get();
 
             for (Element table : doc.select("table[class=yfnc_datamodoutline1]")) {
-                
+
 
                 Elements tableHeaderEles = table.select("th");
                 System.out.println("headers");
@@ -35,9 +52,54 @@ public class TableEg {
                 }
             }
 
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void fetchCSV() {
+        ArrayList<Double> open = new ArrayList<Double>();
+        
+      
+        try {
+            //try {
+                Reader reader = null;
+                InputStream input = null;
+                input = new URL("http://ichart.finance.yahoo.com/table.csv?s=AMD&d=5&e=30&f=2013&g=d&a=2&b=21&c=1983&ignore=.csv").openStream();
+                reader = new InputStreamReader(input, "UTF-8");
+                BufferedReader brReadMe = new BufferedReader(reader);
+                String strLine =brReadMe.readLine();
+                 strLine =brReadMe.readLine();
+                //for all lines
+                while (strLine != null) {
+                    try {
+                        //if line contains "(see also"
+                        String lineEle[] = strLine.split(",");
+                        open.add( Double.parseDouble(lineEle[1]));
+                     
+                        
+                     //   if (strLine.toLowerCase().contains("(see also")) {
+                     //       //write line from "(see also" to ")"
+                     //       int iBegin = strLine.toLowerCase().indexOf("(see also");
+                     //       String strTemp = strLine.substring(iBegin);
+                     //       int iLittleEnd = strTemp.indexOf(")");
+                     //       System.out.println(strLine.substring(iBegin, iBegin + iLittleEnd));
+                     //   }
+
+                        //update line
+                        strLine = brReadMe.readLine();
+                    } //end for
+                    catch (IOException ex) {
+                        Logger.getLogger(TableEg.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } //end for
+                reader.close();
+            //
+        } catch (IOException ex) {
+            Logger.getLogger(TableEg.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
+    }
+    
 }
