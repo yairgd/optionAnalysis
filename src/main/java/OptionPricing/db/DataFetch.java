e7093031f07a4bc418fbec04570ebb2e4b15c4a3
Yair Gadelov <yairg@coloright.com>
2013-07-01 18:29:05 +0000

    fetch option values is done

--- src/main/java/OptionPricing/db/DataFetch.java
+++ src/main/java/OptionPricing/db/DataFetch.java
@@ -1,16 +1,17 @@
 package OptionPricing.db;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
@@ -18,17 +19,32 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class TableEg {
+public class DataFetch {
 
     private Hashtable<String, String> callList = new Hashtable<String, String>();
     private Hashtable<String, String> putList = new Hashtable<String, String>();
+    private ArrayList<Double> open = new ArrayList<Double>();
+    private String stock;
 
     public static void main(String[] args) {
-        TableEg tableEg = new TableEg();
-        tableEg.fetch("AMD");
+        /*
+         DateFormat df = new SimpleDateFormat("mm/dd/yyyy");
+         Date startDate = df.parse(startDate);
+         */
+
+
+
+
+        DataFetch tableEg = new DataFetch("AMD");
+        // tableEg.fetchStock("06/27/2007","06/27/2010");
+        tableEg.fetchOptionData();
     }
 
-    public void fetch(String stock) {
+    public DataFetch(String _stock) {
+        stock = _stock;
+    }
+
+    public void fetchOptionData() {
         //String html = "http://publib.boulder.ibm.com/infocenter/iadthelp/v7r1/topic/" +
         //    "com.ibm.etools.iseries.toolbox.doc/htmtblex.htm";
 
@@ -57,7 +73,7 @@ public class TableEg {
                             putList.put(tds.get(1).text(), tds.get(0).text());
                         }
                     }
-                    
+
                 }
                 is_call = false;
             }
@@ -68,48 +84,90 @@ public class TableEg {
         }
     }
 
-    public void fetchCSV() {
-        ArrayList<Double> open = new ArrayList<Double>();
-        
-      
+    public void fetchStock(String date1, String date2) {
+
         try {
-            //try {
+            DateFormat df = new SimpleDateFormat("mm/dd/yyyy");
+            Date startDate = df.parse(date1);
+            Date endDate = df.parse(date2);
+            try {
+                //try {
                 Reader reader = null;
                 InputStream input = null;
-                input = new URL("http://ichart.finance.yahoo.com/table.csv?s=AMD&d=5&e=30&f=2013&g=d&a=2&b=21&c=1983&ignore=.csv").openStream();
+                input = new URL("http://ichart.finance.yahoo.com/table.csv?s=" + stock + "&d="+startDate.getDay()+"&e=30&f=2013&g=d&a=2&b=21&c=1983&ignore=.csv").openStream();
                 reader = new InputStreamReader(input, "UTF-8");
                 BufferedReader brReadMe = new BufferedReader(reader);
-                String strLine =brReadMe.readLine();
-                 strLine =brReadMe.readLine();
+                String strLine = brReadMe.readLine();
+                strLine = brReadMe.readLine();
                 //for all lines
                 while (strLine != null) {
                     try {
                         //if line contains "(see also"
                         String lineEle[] = strLine.split(",");
-                        open.add( Double.parseDouble(lineEle[1]));
-                     
-                        
-                     //   if (strLine.toLowerCase().contains("(see also")) {
-                     //       //write line from "(see also" to ")"
-                     //       int iBegin = strLine.toLowerCase().indexOf("(see also");
-                     //       String strTemp = strLine.substring(iBegin);
-                     //       int iLittleEnd = strTemp.indexOf(")");
-                     //       System.out.println(strLine.substring(iBegin, iBegin + iLittleEnd));
-                     //   }
+                        getOpen().add(Double.parseDouble(lineEle[1]));
+
+
+                        //   if (strLine.toLowerCase().contains("(see also")) {
+                        //       //write line from "(see also" to ")"
+                        //       int iBegin = strLine.toLowerCase().indexOf("(see also");
+                        //       String strTemp = strLine.substring(iBegin);
+                        //       int iLittleEnd = strTemp.indexOf(")");
+                        //       System.out.println(strLine.substring(iBegin, iBegin + iLittleEnd));
+                        //   }
 
                         //update line
                         strLine = brReadMe.readLine();
                     } //end for
                     catch (IOException ex) {
-                        Logger.getLogger(TableEg.class.getName()).log(Level.SEVERE, null, ex);
+                        Logger.getLogger(DataFetch.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 } //end for
                 reader.close();
-            //
-        } catch (IOException ex) {
-            Logger.getLogger(TableEg.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+                //
+            } catch (IOException ex) {
+                Logger.getLogger(DataFetch.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(DataFetch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+
 
     }
-    
+
+    /**
+     * @return the callList
+     */
+    public Hashtable<String, String> getCallList() {
+        return callList;
+    }
+
+    /**
+     * @return the putList
+     */
+    public Hashtable<String, String> getPutList() {
+        return putList;
+    }
+
+    /**
+     * @return the open
+     */
+    public ArrayList<Double> getOpen() {
+        return open;
+    }
+
+    /**
+     * @return the stock
+     */
+    public String getStock() {
+        return stock;
+    }
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
 }
